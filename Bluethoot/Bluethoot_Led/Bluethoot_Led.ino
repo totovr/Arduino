c78114126d87d89fa93030fd5208d941c6e46a36
totovr <a.vegaramirez76@gmail.com>
2017-11-08 15:17:11 +0900

    Update

--- Bluethoot/Bluethoot_Led/Bluethoot_Led.ino
+++ Bluethoot/Bluethoot_Led/Bluethoot_Led.ino
@@ -1,31 +1,34 @@
-#include <SoftwareSerial.h>
+#include "SimpleBLE.h"
 
-SoftwareSerial HC06(11,10);
-const char DOUT_LED = 2;
-String messageRecu;
+#if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
+#error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
+#endif
+
+SimpleBLE ble;
+
+void onButton(){
+    String out = "BLE32 name: ";
+    out += String(millis() / 1000);
+    Serial.println(out);
+    ble.begin(out);
+}
 
 void setup() {
-Serial.begin(9600);
-HC06.begin(9600);  
-pinMode(DOUT_LED, OUTPUT);
-digitalWrite(DOUT_LED, LOW);
- }
-  
-void loop()
-{
-     while(HC06.available())
-     {
-       delay(3);
-       char c = HC06.read();
-       messageRecu += c;
-     }
-     if (messageRecu.length() >0)
-     {
-       Serial.println(messageRecu);
-       if (messageRecu == "1")     
-        {digitalWrite(DOUT_LED, HIGH);}
-       if (messageRecu == "0")
-        {digitalWrite(DOUT_LED, LOW);}
-       messageRecu="";
-     }
- }
+    Serial.begin(115200);
+    Serial.setDebugOutput(true);
+    pinMode(0, INPUT_PULLUP);
+    Serial.print("ESP32 SDK: ");
+    Serial.println(ESP.getSdkVersion());
+    ble.begin("ESP32 SimpleBLE");
+    Serial.println("Press the button to change the device's name");
+}
+
+void loop() {
+    static uint8_t lastPinState = 1;
+    uint8_t pinState = digitalRead(0);
+    if(!pinState && lastPinState){
+        onButton();
+    }
+    lastPinState = pinState;
+    while(Serial.available()) Serial.write(Serial.read());
+}
