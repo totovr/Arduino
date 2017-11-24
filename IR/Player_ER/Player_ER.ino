24b323c232573d8df0b035a2591a98705989c746
totovr <a.vegaramirez76@gmail.com>
2017-11-24 19:59:22 +0900

    debug

--- IR/Player_ER/Player_ER.ino
+++ IR/Player_ER/Player_ER.ino
@@ -5,22 +5,18 @@ int ledIR =  13;         //pin for IR led
 int lastvalue = 0;     // previous state of the button
 
 //Receptor Variables
-
 #define IRpin_PIN PIND
 #define IRpin 2
- 
 // the maximum pulse we'll listen for - 65 milliseconds is a long time
 #define MAXPULSE 65000
- 
-// what our timing resolution should be, larger is better
+ // what our timing resolution should be, larger is better
 // as its more 'precise' - but too large and you wont get
 // accurate timing
 #define RESOLUTION 20
- 
-// we will store up to 100 pulse pairs (this is -a lot-)
+ // we will store up to 100 pulse pairs (this is -a lot-)
 uint16_t pulses[100][2]; // pair is high and low pulse
+uint16_t highpulse, lowpulse; // temporary storage timing
 uint8_t currentpulse = 0; // index for pulses we're storing
-
 int counter;
 
 void setup() 
@@ -29,29 +25,53 @@ void setup()
   Serial.begin(9600);      //Start serial communication
 }
 
-void loop() {
+void loop() 
+{
   value = digitalRead(inputPin);  //read the digital input
   PullUp();
+  Pulse();
 
-  uint16_t highpulse, lowpulse; // temporary storage timing
+}
+
+void Pulse(void)
+{
   highpulse = lowpulse = 0; // start out with no pulse length
  
-  // while (digitalRead(IRpin)) { // this is too slow!
-  while (IRpin_PIN & (1 << IRpin)) {
-   // pin is still HIGH
-
-   // count off another few microseconds
-   highpulse++;
-   delayMicroseconds(RESOLUTION);
-
-   // If the pulse is too long, we 'timed out' - either nothing
-   // was received or the code is finished, so print what
-   // we've grabbed so far, and then reset
-   if ((highpulse >= MAXPULSE) && (currentpulse != 0)) {
-     printpulses();
-     currentpulse=0;
-     return;
-
+    // while (digitalRead(IRpin)) { // this is too slow!
+    while (IRpin_PIN & (1 << IRpin)) {
+     // pin is still HIGH
+ 
+     // count off another few microseconds
+     highpulse++;
+     delayMicroseconds(RESOLUTION);
+ 
+     // If the pulse is too long, we 'timed out' - either nothing
+     // was received or the code is finished, so print what
+     // we've grabbed so far, and then reset
+     if ((highpulse >= MAXPULSE) && (currentpulse != 0)) {
+       printpulses();
+       currentpulse=0;
+       return;
+     }
+  }
+  // we didn't time out so lets stash the reading
+  pulses[currentpulse][0] = highpulse;
+ 
+  // same as above
+  while (! (IRpin_PIN & _BV(IRpin))) {
+     // pin is still LOW
+     lowpulse++;
+     delayMicroseconds(RESOLUTION);
+     if ((lowpulse >= MAXPULSE) && (currentpulse != 0)) {
+       printpulses();
+       currentpulse=0;
+       return;
+     }
+  }
+  pulses[currentpulse][1] = lowpulse;
+ 
+  // we read one high-low pulse successfully, continue!
+  currentpulse++;  
 }
 
 //Pulse Generation
