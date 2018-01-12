ae146afb8736aa1f97ae1dcf32847c1a6b0bd251
totovr <a.vegaramirez76@gmail.com>
2018-01-12 20:41:31 +0900

    New features added
    EMG sensor with IR

--- SH/Game_NBeta/Player/src/Player_ER.ino
+++ SH/Game_NBeta/Player/src/Player_ER.ino
@@ -1,9 +1,10 @@
+//Libraries
+#include <IRremote.h>
 //code values
 const int inputPin = 2;  //pin for pullup resistor
 int value = 0;
 int ledLaser =  13;         //pin for IR led
 int lastvalue = 0;     // previous state of the button
-
 //decode
 //counter to begin the game
 int count = 0;
@@ -11,11 +12,16 @@ int count = 0;
 int points = 0;
 const int analogInPin = A0;
 int LaserValue = 0;
-
 int PointC = A1;//By this pin we will send the data to the OLED display for points
-
+//EMG Sensor
+int emg = 0;
+int emgPin = A2;
+int emg_counter = 0;
+//Infrared
+IRsend irsend;
+const int analogPin = A3;
+int outputvalue = 0;
 //Death variable
-
 int end = 0;
 
 void setup() {
@@ -29,9 +35,12 @@ void loop() {
   PullUp();
   LaserValue = analogRead(analogInPin);
   delay(10);
-//  Serial.print(LaserValue);
-//  Serial.print("\n");
+  //  Serial.print(LaserValue);
+  //  Serial.print("\n");
   Points();
+  //EMG sensor reading
+  emg = analogRead(emgPin);
+  EMG();
 }
 //Encode
 void PullUp() {
@@ -52,14 +61,14 @@ void SendPluse() {
   delay(2);
 }
 //decode
-void Points(){
+void Points() {
   if(LaserValue > 180) {
     if(end < 20) {
       end=+1;
       digitalWrite(PointC, HIGH);
       delay(1000);
       digitalWrite(PointC, LOW);
-    }//If we detect that the end variable is over 20 we will do a infinit bucle
+    }//If we detect that the end variable is over 20 we will do a infinite bucle
     else {
       while(1) {
         digitalWrite(PointC, HIGH);
@@ -67,4 +76,15 @@ void Points(){
     }
   }
   LaserValue = 0;
+}
+
+void EMG() {
+  if(emg > 4000) {
+    if(emg_counter = 20) {
+      //outputvalue = analogRead(analogPin);
+      irsend.sendSony(0x68B90, 20);
+      delay(100);
+      emg_counter = 0;
+    }
+  }
 }
