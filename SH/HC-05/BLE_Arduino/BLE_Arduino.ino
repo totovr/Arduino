92bc9a3ff5dacbad35161780e4e680f756e5be71
Antonio Vega <a.vegaramirez76@gmail.com>
2018-01-18 14:57:31 +0900

    Update BLE_Arduino.ino

--- SH/HC-05/BLE_Arduino/BLE_Arduino.ino
+++ SH/HC-05/BLE_Arduino/BLE_Arduino.ino
@@ -1,29 +1,28 @@
-#include<SoftwareSerial.h>
-SoftwareSerial BTSerial(10, 11); // RT|TX
+#include <SoftwareSerial.h>// import the serial library
 
-int ledPin =  7;
-int state = 0;
+SoftwareSerial Genotronex(10, 11); // RX, TX
+int ledpin=13; // led on D13 will show blink on / off
+int BluetoothData; // the data given from Computer
 
 void setup() {
-  pinMode(ledPin, OUTPUT);
-  digitalWrite(ledPin, LOW);
-  Serial.begin(9600);
-  Serial.println("Ready!");
-  BTSerial.begin(38400); // Default communication rate of the Bluetooth module
+  // put your setup code here, to run once:
+  Genotronex.begin(9600);
+  Genotronex.println("Bluetooth On please press 1 or 0 blink LED ..");
+  pinMode(ledpin,OUTPUT);
 }
 
 void loop() {
-  if(BTSerial.available() > 0){ // Checks whether data is comming from the serial port
-    state = BTSerial.read(); // Reads the data from the serial port
- }
- if (state == '0') {
-  digitalWrite(ledPin, LOW); // Turn LED OFF
-  Serial.println("LED: OFF"); // Send back, to the phone, the String "LED: ON"
-  state = 0;
- }
- else if (state == '1') {
-  digitalWrite(ledPin, HIGH);
-  Serial.println("LED: ON");;
-  state = 0;
- } 
+  // put your main code here, to run repeatedly:
+   if (Genotronex.available()){
+BluetoothData=Genotronex.read();
+   if(BluetoothData=='1'){   // if number 1 pressed ....
+   digitalWrite(ledpin,1);
+   Genotronex.println("LED  On D13 ON ! ");
+   }
+  if (BluetoothData=='0'){// if number 0 pressed ....
+  digitalWrite(ledpin,0);
+   Genotronex.println("LED  On D13 Off ! ");
+  }
+}
+delay(100);// prepare for next data ...
 }
