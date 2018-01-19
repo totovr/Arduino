31c7d935165f5de27a2a9f511e1d6683e197e777
totovr <a.vegaramirez76@gmail.com>
2018-01-19 17:34:45 +0900

    UPDATE

---
+++ SH/Game_NBeta/MASTER_SLAVE/MASTER/MASTER.ino
@@ -0,0 +1,22 @@
+#define ledPin 9
+int state = 0;
+int potValue = 0;
+void setup() {
+  pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin, LOW);
+  Serial.begin(9600); // Default communication rate of the Bluetooth module
+}
+void loop() {
+ if(Serial.available() > 0){ // Checks whether data is comming from the serial port
+    state = Serial.read(); // Reads the data from the serial port
+ }
+ // Controlling the LED
+ if (state == '1') {
+  digitalWrite(ledPin, HIGH); // LED ON
+  state = 0;
+ }
+ else if (state == '0') {
+  digitalWrite(ledPin, LOW); // LED ON
+  state = 0;
+ }
+}
