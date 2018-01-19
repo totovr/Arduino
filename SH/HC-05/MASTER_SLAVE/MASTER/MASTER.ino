5eccaf64a9ccdaa9f923788dc2a09f1e1d53aadf
Antonio Vega <a.vegaramirez76@gmail.com>
2018-01-19 19:11:06 +0900

    Update MASTER.ino

--- SH/HC-05/MASTER_SLAVE/MASTER/MASTER.ino
+++ SH/HC-05/MASTER_SLAVE/MASTER/MASTER.ino
@@ -1,10 +1,10 @@
 #define ledPin 9
 int state = 0;
-int potValue = 0;
+
 void setup() {
   pinMode(ledPin, OUTPUT);
   digitalWrite(ledPin, LOW);
-  Serial.begin(9600); // Default communication rate of the Bluetooth module
+  Serial.begin(38400); // Default communication rate of the Bluetooth module
 }
 void loop() {
  if(Serial.available() > 0){ // Checks whether data is comming from the serial port
@@ -13,10 +13,8 @@ void loop() {
  // Controlling the LED
  if (state == '1') {
   digitalWrite(ledPin, HIGH); // LED ON
+  delay(500);
   state = 0;
- }
- else if (state == '0') {
-  digitalWrite(ledPin, LOW); // LED ON
-  state = 0;
- }
+  digitalWrite(ledPin, LOW); // LED OFF
+  }
 }
